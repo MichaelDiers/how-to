fe1372863b5e208486276dd8b5b925a4b25e5d2a
MichaelDiers <md@sreid.de>
2024-12-30 14:40:36 +0100

    switch to xunit v3, add skip tests

---
+++ .net/xUnit.net/HowToXUnit/ClassLibrary.Tests/UsingSkip.cs
@@ -0,0 +1,44 @@
+﻿namespace ClassLibrary.Tests;
+
+public class UsingSkip(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void Skip()
+    {
+        // do some tests
+
+        Assert.Skip("Test is skipped here");
+
+        Assert.Fail("Test should not reach");
+    }
+
+    [Fact]
+    public void SkipUnless()
+    {
+        Assert.SkipUnless(
+            true,
+            "Test continues");
+
+        testOutputHelper.WriteLine("test should pass here");
+
+        Assert.SkipUnless(
+            false,
+            "Test is skipped here");
+
+        Assert.Fail("Test should not reach");
+    }
+
+    [Fact]
+    public void SkipWhen()
+    {
+        Assert.SkipWhen(
+            false,
+            "Test continues");
+
+        Assert.SkipWhen(
+            true,
+            "Test is skipped here");
+
+        Assert.Fail("Test should not reach");
+    }
+}
