392b7c22a233d6ac94d0bd54005e4ee6119ba6f3
MichaelDiers <md@sreid.de>
2024-12-30 15:37:52 +0100

    add explicit

---
+++ .net/xUnit.net/HowToXUnit/ClassLibrary.Tests/UsingExplicit.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary.Tests;
+
+public class UsingExplicit
+{
+    [Fact(Explicit = true)]
+    public void ExplicitTestShouldNotRun()
+    {
+        // the test explorer starts this test if only this test is selected
+        // cli flag exists
+        Assert.Fail("Should not run");
+    }
+
+    [Fact(Explicit = false)]
+    public void NonExplicitTestShouldRun()
+    {
+        // do some tests
+    }
+}
