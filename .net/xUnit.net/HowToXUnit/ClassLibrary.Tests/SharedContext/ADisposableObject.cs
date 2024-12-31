5a3228fd8cc5cff9b706219f21ee042f380ecd46
MichaelDiers <md@sreid.de>
2024-12-31 11:38:40 +0100

    add shared context test

---
+++ .net/xUnit.net/HowToXUnit/ClassLibrary.Tests/SharedContext/ADisposableObject.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary.Tests.SharedContext;
+
+internal class ADisposableObject : IDisposable
+{
+    public Guid Guid { get; } = Guid.NewGuid();
+
+    /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+    public void Dispose()
+    {
+        // dispose something
+    }
+}
---
+++ .net/xUnit.net/HowToXUnit/ClassLibrary.Tests/SharedContext/ConstructorAndDispose.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassLibrary.Tests.SharedContext;
+
+public class ConstructorAndDispose(ITestOutputHelper testOutputHelper) : IDisposable
+{
+    private readonly ADisposableObject aDisposableObject = new();
+
+    public void Dispose()
+    {
+        this.aDisposableObject.Dispose();
+    }
+
+    /// <summary>
+    ///     Tests <see cref="TestA" /> and <see cref="TestB" /> do not share the same <see cref="aDisposableObject" />: For
+    ///     each test in a class a new instance of the class is created.
+    /// </summary>
+    [Fact]
+    public void TestA()
+    {
+        testOutputHelper.WriteLine(this.aDisposableObject.Guid.ToString());
+    }
+
+    /// <summary>
+    ///     Tests <see cref="TestA" /> and <see cref="TestB" /> do not share the same <see cref="aDisposableObject" />: For
+    ///     each test in a class a new instance of the class is created.
+    /// </summary>
+    [Fact]
+    public void TestB()
+    {
+        testOutputHelper.WriteLine(this.aDisposableObject.Guid.ToString());
+    }
+}
