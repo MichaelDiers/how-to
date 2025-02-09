b6aa0c97f85e8e3605244c7da57302b5d8aa1f32
MichaelDiers <md@sreid.de>
2025-02-09 23:34:08 +0100

    add watermark for combobox, textbox and passwordbox

---
+++ .net/HowToWpf/Md.Libs.Wpf/DependencyProperties/Watermark/WatermarkAdorner.cs
@@ -0,0 +1,160 @@
+﻿namespace Md.Libs.Wpf.DependencyProperties.Watermark;
+
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+/// <summary>
+///     Add a watermark to an element.
+/// </summary>
+/// <seealso cref="System.Windows.Documents.Adorner" />
+internal class WatermarkAdorner : Adorner
+{
+    /// <summary>
+    ///     The text of the watermark.
+    /// </summary>
+    private readonly string watermark;
+
+    /// <summary>Initializes a new instance of the <see cref="T:System.Windows.Documents.Adorner" /> class.</summary>
+    /// <param name="adornedElement">The element to bind the adorner to.</param>
+    /// <param name="watermark">The text of the watermark.</param>
+    /// <exception cref="T:System.ArgumentNullException">adornedElement is <see langword="null" />.</exception>
+    public WatermarkAdorner(UIElement adornedElement, string watermark)
+        : base(adornedElement)
+    {
+        this.IsHitTestVisible = false;
+        this.watermark = watermark;
+    }
+
+    /// <summary>
+    ///     When overridden in a derived class, participates in rendering operations that are directed by the layout
+    ///     system. The rendering instructions for this element are not used directly when this method is invoked, and are
+    ///     instead preserved for later asynchronous use by layout and drawing.
+    /// </summary>
+    /// <param name="drawingContext">
+    ///     The drawing instructions for a specific element. This context is provided to the layout
+    ///     system.
+    /// </param>
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+        base.OnRender(drawingContext);
+
+        if (this.AdornedElement is not Control control)
+        {
+            return;
+        }
+
+        var formattedText = new FormattedText(
+            this.watermark,
+            CultureInfo.CurrentUICulture,
+            FlowDirection.LeftToRight,
+            new Typeface(control.FontFamily.Source),
+            control.FontSize,
+            new SolidColorBrush(Colors.Gray),
+            new NumberSubstitution(),
+            VisualTreeHelper.GetDpi(control).PixelsPerDip);
+
+        formattedText.SetFontStyle(FontStyles.Italic);
+
+        var origin = control is ComboBox comboBox
+            ? WatermarkAdorner.CalculateOrigin(
+                comboBox,
+                formattedText)
+            : WatermarkAdorner.CalculateOrigin(
+                control,
+                formattedText);
+
+        drawingContext.DrawText(
+            formattedText,
+            origin);
+    }
+
+    /// <summary>
+    ///     Calculate the origin of the watermark text <paramref name="formattedText" />.
+    /// </summary>
+    /// <param name="control">The control that adorner layer is used.</param>
+    /// <param name="formattedText">The <see cref="FormattedText" /> of the watermark.</param>
+    /// <returns>The calculated origin of the watermark.</returns>
+    /// <remarks>
+    ///     A <see cref="ComboBox" /> uses a control for displaying the selected element. The calculation does not include
+    ///     the margin and padding of this inner control.
+    /// </remarks>
+    private static Point CalculateOrigin(ComboBox control, FormattedText formattedText)
+    {
+        var origin = WatermarkAdorner.CalculateOrigin(
+            control as Control,
+            formattedText);
+
+        var x = origin.X;
+        switch (control.HorizontalContentAlignment)
+        {
+            case HorizontalAlignment.Left:
+            case HorizontalAlignment.Stretch:
+                x += control.Padding.Left;
+                break;
+            case HorizontalAlignment.Center:
+                break;
+            case HorizontalAlignment.Right:
+                x -= control.Padding.Right;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        // a combo box ignores the vertical content alignment
+        var y = control.RenderSize.Height / 2 - formattedText.Height / 2;
+
+        return new Point(
+            x,
+            y);
+    }
+
+    /// <summary>
+    ///     Calculate the origin of the watermark text <paramref name="formattedText" />.
+    /// </summary>
+    /// <param name="control">The control that adorner layer is used.</param>
+    /// <param name="formattedText">The <see cref="FormattedText" /> of the watermark.</param>
+    /// <returns>The calculated origin of the watermark.</returns>
+    private static Point CalculateOrigin(Control control, FormattedText formattedText)
+    {
+        double x;
+        switch (control.HorizontalContentAlignment)
+        {
+            case HorizontalAlignment.Left:
+            case HorizontalAlignment.Stretch:
+                x = control.Padding.Left;
+                break;
+            case HorizontalAlignment.Center:
+                x = control.RenderSize.Width / 2 - formattedText.Width / 2;
+                break;
+            case HorizontalAlignment.Right:
+                x = control.RenderSize.Width - control.Padding.Right - formattedText.Width;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        double y;
+        switch (control.VerticalContentAlignment)
+        {
+            case VerticalAlignment.Stretch:
+            case VerticalAlignment.Top:
+                y = control.Padding.Top;
+                break;
+            case VerticalAlignment.Center:
+                y = control.RenderSize.Height / 2 - formattedText.Height / 2;
+                break;
+            case VerticalAlignment.Bottom:
+                y = control.RenderSize.Height - formattedText.Height - control.Padding.Bottom;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        return new Point(
+            x,
+            y);
+    }
+}
---
+++ .net/HowToWpf/Md.Libs.Wpf/DependencyProperties/Watermark/WatermarkDependencyProperty.cs
@@ -0,0 +1,233 @@
+﻿namespace Md.Libs.Wpf.DependencyProperties.Watermark;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+/// <summary>
+///     Add a watermark to <see cref="ComboBox" />, <see cref="PasswordBox" /> and <see cref="TextBox" />.
+/// </summary>
+public static class WatermarkDependencyProperty
+{
+    /// <summary>
+    ///     The attached watermark dependency property.
+    /// </summary>
+    public static DependencyProperty WatermarkProperty = DependencyProperty.RegisterAttached(
+        nameof(WatermarkDependencyProperty.WatermarkProperty)[..^8],
+        typeof(string),
+        typeof(WatermarkDependencyProperty),
+        new PropertyMetadata(WatermarkDependencyProperty.OnPropertyChanged));
+
+    /// <summary>
+    ///     Gets the text of the watermark.
+    /// </summary>
+    /// <param name="obj">The object to that the watermark is attached.</param>
+    /// <returns>The text of the watermark.</returns>
+    public static string GetWatermark(DependencyObject obj)
+    {
+        return (string) obj.GetValue(WatermarkDependencyProperty.WatermarkProperty);
+    }
+
+    /// <summary>
+    ///     Sets the text of the watermark.
+    /// </summary>
+    /// <param name="obj">The object to that the watermark is attached.</param>
+    /// <param name="watermark">The text of the watermark.</param>
+    public static void SetWatermark(DependencyObject obj, string watermark)
+    {
+        obj.SetValue(
+            WatermarkDependencyProperty.WatermarkProperty,
+            watermark);
+    }
+
+    /// <summary>
+    ///     Adds the watermark adorner to the <paramref name="control" />.
+    /// </summary>
+    /// <param name="control">The control to that the <see cref="WatermarkAdorner" /> is added.</param>
+    private static void AddWatermark(Control control)
+    {
+        // check if the control has an adorner layer
+        var adornerLayer = AdornerLayer.GetAdornerLayer(control);
+        if (adornerLayer is null)
+        {
+            return;
+        }
+
+        // check if the adorner is already added
+        var adorners = adornerLayer.GetAdorners(control);
+        if (adorners is not null && adorners.Any(adorner => adorner.GetType() == typeof(WatermarkAdorner)))
+        {
+            return;
+        }
+
+        // add the adorner
+        adornerLayer.Add(
+            new WatermarkAdorner(
+                control,
+                WatermarkDependencyProperty.GetWatermark(control)));
+    }
+
+    /// <summary>
+    ///     Checks the display conditions of the <see cref="WatermarkAdorner" /> for the supported controls. Handles adding and
+    ///     removing the <see cref="WatermarkAdorner" />.
+    /// </summary>
+    /// <param name="sender">The sender that has raised an event that can change the display condition of the watermark.</param>
+    private static void HandleWatermark(object? sender)
+    {
+        // reject non-controls
+        if (sender is not Control control)
+        {
+            return;
+        }
+
+        // reject if adorner layer is not available
+        var adornerLayer = AdornerLayer.GetAdornerLayer(control);
+        if (adornerLayer is null)
+        {
+            return;
+        }
+
+        // control is not visible
+        if (!control.IsVisible)
+        {
+            WatermarkDependencyProperty.RemoveWatermark(control);
+            return;
+        }
+
+        // check display conditions for controls
+        var addWatermark = control is ComboBox {IsDropDownOpen: false, SelectedItem: null}
+            or TextBox {IsFocused: false, Text: null or ""}
+            or PasswordBox {IsFocused: false, Password: null or ""};
+
+        // add the watermark
+        if (addWatermark)
+        {
+            WatermarkDependencyProperty.AddWatermark(control);
+        }
+        else
+        {
+            // remove the watermark
+            WatermarkDependencyProperty.RemoveWatermark(control);
+        }
+    }
+
+    /// <summary>
+    ///     Called when an event is raised that could change the watermark visibility.
+    /// </summary>
+    /// <param name="sender">The sender that raised the event.</param>
+    /// <param name="e">The <see cref="System.EventArgs" /> instance containing the event data.</param>
+    private static void OnHandleEvent(object sender, EventArgs e)
+    {
+        WatermarkDependencyProperty.HandleWatermark(sender);
+    }
+
+    /// <summary>
+    ///     Called when an event is raised that could change the watermark visibility.
+    /// </summary>
+    /// <param name="sender">The sender that raised the event.</param>
+    /// <param name="e">
+    ///     The <see cref="System.Windows.DependencyPropertyChangedEventArgs" /> instance containing the event
+    ///     data.
+    /// </param>
+    private static void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        WatermarkDependencyProperty.HandleWatermark(sender);
+    }
+
+    /// <summary>
+    ///     Called when the dependency property <see cref="WatermarkProperty" /> changed.
+    /// </summary>
+    /// <param name="d">The dependency object to the <see cref="WatermarkProperty" /> is attached.</param>
+    /// <param name="e">
+    ///     The <see cref="System.Windows.DependencyPropertyChangedEventArgs" /> instance containing the event
+    ///     data.
+    /// </param>
+    private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not Control control)
+        {
+            return;
+        }
+
+        control.Loaded += WatermarkDependencyProperty.OnHandleEvent;
+        control.Unloaded += WatermarkDependencyProperty.OnUnloaded;
+
+        control.IsVisibleChanged += WatermarkDependencyProperty.OnIsVisibleChanged;
+
+        if (control is ComboBox comboBox)
+        {
+            comboBox.SelectionChanged += WatermarkDependencyProperty.OnHandleEvent;
+        }
+        else if (control is PasswordBox passwordBox)
+        {
+            passwordBox.GotFocus += WatermarkDependencyProperty.OnHandleEvent;
+            passwordBox.LostFocus += WatermarkDependencyProperty.OnHandleEvent;
+            passwordBox.PasswordChanged += WatermarkDependencyProperty.OnHandleEvent;
+        }
+        else if (control is TextBox textBox)
+        {
+            textBox.GotFocus += WatermarkDependencyProperty.OnHandleEvent;
+            textBox.LostFocus += WatermarkDependencyProperty.OnHandleEvent;
+            textBox.TextChanged += WatermarkDependencyProperty.OnHandleEvent;
+        }
+    }
+
+    /// <summary>
+    ///     Called when the control to that the <see cref="WatermarkProperty" /> is attached is unloaded.
+    /// </summary>
+    /// <param name="sender">The sender that raised the event.</param>
+    /// <param name="e">The <see cref="System.Windows.RoutedEventArgs" /> instance containing the event data.</param>
+    private static void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is not Control control)
+        {
+            return;
+        }
+
+        control.Loaded -= WatermarkDependencyProperty.OnHandleEvent;
+        control.Unloaded -= WatermarkDependencyProperty.OnUnloaded;
+
+        control.IsVisibleChanged -= WatermarkDependencyProperty.OnIsVisibleChanged;
+
+        if (control is ComboBox comboBox)
+        {
+            comboBox.SelectionChanged -= WatermarkDependencyProperty.OnHandleEvent;
+        }
+        else if (control is TextBox textBox)
+        {
+            textBox.GotFocus -= WatermarkDependencyProperty.OnHandleEvent;
+            textBox.LostFocus -= WatermarkDependencyProperty.OnHandleEvent;
+            textBox.TextChanged -= WatermarkDependencyProperty.OnHandleEvent;
+        }
+        else if (control is PasswordBox passwordBox)
+        {
+            passwordBox.GotFocus -= WatermarkDependencyProperty.OnHandleEvent;
+            passwordBox.LostFocus -= WatermarkDependencyProperty.OnHandleEvent;
+            passwordBox.PasswordChanged -= WatermarkDependencyProperty.OnHandleEvent;
+        }
+    }
+
+    /// <summary>
+    ///     Removes the watermark from the adorner layer.
+    /// </summary>
+    /// <param name="control">The control from that the adorner is removed.</param>
+    private static void RemoveWatermark(Control control)
+    {
+        var adornerLayer = AdornerLayer.GetAdornerLayer(control);
+        if (adornerLayer is null)
+        {
+            return;
+        }
+
+        var adorners = adornerLayer.GetAdorners(control);
+        if (adorners is null)
+        {
+            return;
+        }
+
+        foreach (var adorner in adorners.Where(adorner => adorner.GetType() == typeof(WatermarkAdorner)))
+        {
+            adornerLayer.Remove(adorner);
+        }
+    }
+}
