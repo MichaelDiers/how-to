5d00843b58d14ba46cef64ff3b8b797d1ac64c1f
MichaelDiers <md@sreid.de>
2025-02-10 08:38:37 +0100

    add a custom configuration builder

---
+++ .net/HowToWpf/Md.Libs.Wpf.Tests/Configuration/ApplicationConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Md.Libs.Wpf.Tests.Configuration;
+
+/// <summary>
+///     The application configuration that describes the <c>appsettings.json</c> file.
+/// </summary>
+/// <param name="key">The value of the <see cref="Key" /> property.</param>
+internal class ApplicationConfiguration(string key)
+{
+    /// <summary>
+    ///     Gets the key value.
+    /// </summary>
+    public string Key => key;
+}
