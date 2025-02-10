014c57d99370f46883f3b6baaddb1a836abf8943
MichaelDiers <md@sreid.de>
2025-02-11 00:30:27 +0100

    add small and large command buttons

---
+++ .net/HowToWpf/Md.Libs.Wpf/Controls/CancelCommandOverlay.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Md.Libs.Wpf.Controls
+{
+    /// <summary>
+    /// Interaction logic for CancelCommandOverlay.xaml
+    /// </summary>
+    public partial class CancelCommandOverlay : UserControl
+    {
+        public CancelCommandOverlay()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ .net/HowToWpf/Md.Libs.Wpf/DependencyProperties/CommandButton/SmallCommandButtonImageDependencyProperty.cs
@@ -0,0 +1,62 @@
+﻿namespace Md.Libs.Wpf.DependencyProperties.CommandButton;
+
+using System.Windows;
+using System.Windows.Controls;
+
+/// <summary>
+///     Attaches an image property to a control. Sets the style of the control if the control is a <see cref="Button" />.
+/// </summary>
+public static class SmallCommandButtonImageDependencyProperty
+{
+    /// <summary>
+    ///     The resource name of the <see cref="Button.Style" />.
+    /// </summary>
+    private const string SmallCommandButtonStyle =
+        nameof(SmallCommandButtonImageDependencyProperty.SmallCommandButtonStyle);
+
+    /// <summary>
+    ///     The attached image property.
+    /// </summary>
+    public static DependencyProperty ImageProperty = DependencyProperty.RegisterAttached(
+        "Image",
+        typeof(string),
+        typeof(SmallCommandButtonImageDependencyProperty),
+        new PropertyMetadata(SmallCommandButtonImageDependencyProperty.PropertyChangedCallback));
+
+    /// <summary>
+    ///     Gets the value of the image property.
+    /// </summary>
+    /// <param name="obj">The property is attached to this <see cref="DependencyObject" />.</param>
+    /// <returns>The path to the image.</returns>
+    public static string GetImage(DependencyObject obj)
+    {
+        return (string) obj.GetValue(SmallCommandButtonImageDependencyProperty.ImageProperty);
+    }
+
+    /// <summary>
+    ///     Sets the value of the image property.
+    /// </summary>
+    /// <param name="obj">The property is attached to this <see cref="DependencyObject" />.</param>
+    /// <param name="value">The new path of the image.</param>
+    public static void SetImage(DependencyObject obj, string value)
+    {
+        obj.SetValue(
+            SmallCommandButtonImageDependencyProperty.ImageProperty,
+            value);
+    }
+
+    /// <summary>
+    ///     Called if <see cref="ImageProperty" /> changed.
+    /// </summary>
+    /// <param name="d">The <see cref="DependencyObject" /> the <see cref="DependencyProperty" /> is attached to.</param>
+    /// <param name="e">The event args.</param>
+    private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is Button button &&
+            Application.Current.FindResource(SmallCommandButtonImageDependencyProperty.SmallCommandButtonStyle) is Style
+                style)
+        {
+            button.Style = style;
+        }
+    }
+}
