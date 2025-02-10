5618ccd4cd8a868277f5180425a5f071402d3b56
MichaelDiers <md@sreid.de>
2025-02-10 08:05:52 +0100

    add dependency injection

---
+++ .net/HowToWpf/Md.Libs.Wpf.Tests/DependencyInjection/ServiceProviderBuilderTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Md.Libs.Wpf.Tests.DependencyInjection;
+
+using Md.Libs.Wpf.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+///     Test for <see cref="ServiceProviderBuilder" />.
+/// </summary>
+public class ServiceProviderBuilderTests
+{
+    /// <summary>
+    ///     The initialized <see cref="IServiceProvider" />.
+    /// </summary>
+    private readonly IServiceProvider serviceProvider =
+        ServiceProviderBuilder.Build(ServiceCollectionExtensions.AddDependencies);
+
+    /// <summary>
+    ///     Try to get a service of a given type.
+    /// </summary>
+    [Fact]
+    public void GetRequiredService()
+    {
+        var service = this.serviceProvider.GetRequiredService<ITestService>();
+
+        Assert.NotNull(service);
+    }
+}
---
+++ .net/HowToWpf/Md.Libs.Wpf/DependencyInjection/ServiceProviderBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace Md.Libs.Wpf.DependencyInjection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+///     Create a <see cref="ServiceProvider" /> that is used for dependency injection.
+/// </summary>
+public static class ServiceProviderBuilder
+{
+    /// <summary>
+    ///     Build a new <see cref="ServiceProvider" /> and add the given dependencies <paramref name="addDependencies" /> to
+    ///     the provider.
+    /// </summary>
+    /// <param name="addDependencies">Dependencies that are added to the new <see cref="ServiceProvider" />.</param>
+    /// <returns>The initialized <see cref="ServiceProvider" />.</returns>
+    public static IServiceProvider Build(params Func<IServiceCollection, IServiceCollection>[] addDependencies)
+    {
+        var services = new ServiceCollection();
+        foreach (var addDependency in addDependencies)
+        {
+            addDependency(services);
+        }
+
+        return services.BuildServiceProvider();
+    }
+}
