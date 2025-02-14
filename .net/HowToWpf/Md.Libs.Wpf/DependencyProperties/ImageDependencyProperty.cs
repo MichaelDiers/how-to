ff20a00f20b0a4c893e1363f95861ea56c030aff
MichaelDiers <md@sreid.de>
2025-02-14 10:01:39 +0100

    add image dependency property

---
+++ .net/HowToWpf/Md.Libs.Wpf/DependencyProperties/ImageDependencyProperty.cs
@@ -0,0 +1,41 @@
+﻿namespace Md.Libs.Wpf.DependencyProperties;
+
+using System.Windows;
+using System.Windows.Media;
+
+/// <summary>
+///     An attached <see cref="DependencyProperty" /> that adds an image property.
+/// </summary>
+public static class ImageDependencyProperty
+{
+    /// <summary>
+    ///     The image <see cref="DependencyProperty" />.
+    /// </summary>
+    public static readonly DependencyProperty ImageProperty = DependencyProperty.RegisterAttached(
+        "Image",
+        typeof(ImageSource),
+        typeof(ImageDependencyProperty),
+        new PropertyMetadata(default(ImageSource)));
+
+    /// <summary>
+    ///     Gets the value of the image property.
+    /// </summary>
+    /// <param name="element">The element to that the <see cref="DependencyProperty" /> is attached to.</param>
+    /// <returns>The value of the <see cref="DependencyProperty" />.</returns>
+    public static ImageSource GetImage(DependencyObject element)
+    {
+        return (ImageSource) element.GetValue(ImageDependencyProperty.ImageProperty);
+    }
+
+    /// <summary>
+    ///     Sets the value of the image property.
+    /// </summary>
+    /// <param name="element">The element to that the <see cref="DependencyProperty" /> is attached to.</param>
+    /// <param name="value">The new value of the <see cref="DependencyProperty" />.</param>
+    public static void SetImage(DependencyObject element, ImageSource value)
+    {
+        element.SetValue(
+            ImageDependencyProperty.ImageProperty,
+            value);
+    }
+}
